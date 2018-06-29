10967409bb4e984573f8aca97cef385e4ab64137
Oliver Nagy <olitheolix@gmail.com>
2018-06-29 23:36:45 +1000

    Flattened the "Watch().stream(...)" syntax to just "Watch(...)"

--- aiokubernetes/watch/watch_test.py
+++ aiokubernetes/watch/watch_test.py
@@ -16,10 +16,42 @@ import json
 
 from asynctest import CoroutineMock, Mock, TestCase
 
-from aiokubernetes.watch import Watch
+import aiokubernetes as k8s
 
 
 class WatchTest(TestCase):
+
+    def test_find_return_type(self):
+        """A few basic test cases to ensure it does what it says."""
+        fun = k8s.watch.watch._find_return_type
+
+        # Must return None because the `None` object has an empty doc string
+        # and is not a K8s type.
+        self.assertIsNone(fun(None))
+
+        # These have non-empty doc strings but are not a K8s types. The
+        # function must therefore return None for them.
+        self.assertIsNone(fun(''))
+        self.assertIsNone(fun(object))
+
+        class ClassWithValidDocString():
+            """This has a valid doc string.
+            :return: V1NamespaceList
+            """
+            pass
+
+        self.assertEqual(fun(ClassWithValidDocString()), 'V1Namespace')
+
+        class ClassWithInvalidDocString():
+            """Multiple :return: strings.
+            :return: foo
+            blah
+            :return: bar
+            """
+            pass
+
+        with self.assertRaises(AssertionError):
+            fun(ClassWithInvalidDocString())
 
     async def test_watch_with_decode(self):
         fake_resp = CoroutineMock()
@@ -42,9 +74,9 @@ class WatchTest(TestCase):
         fake_api.get_namespaces = CoroutineMock(return_value=fake_resp)
         fake_api.get_namespaces.__doc__ = ':return: V1NamespaceList'
 
-        watch = Watch()
+        watch = k8s.watch.Watch(fake_api.get_namespaces, resource_version='123')
         count = 0
-        async for e in watch.stream(fake_api.get_namespaces, resource_version='123'):
+        async for e in watch:
             self.assertEqual("ADDED", e['type'])
             # make sure decoder worked and we got a model with the right name
             self.assertEqual("test%d" % count, e['object'].metadata.name)
@@ -69,8 +101,14 @@ class WatchTest(TestCase):
         fake_resp = CoroutineMock()
         fake_resp.content.readline = CoroutineMock()
         side_effects = [
-            {"type": "ADDED", "object": {"metadata": {"name": "test0"}, "spec": {}, "status": {}}},
-            {"type": "ADDED", "object": {"metadata": {"name": "test1"}, "spec": {}, "status": {}}},
+            {
+                "type": "ADDED",
+                "object": {"metadata": {"name": "test0"}, "spec": {}, "status": {}}
+            },
+            {
+                "type": "ADDED",
+                "object": {"metadata": {"name": "test1"}, "spec": {}, "status": {}}
+            },
         ]
         side_effects = [json.dumps(_).encode('utf8') for _ in side_effects]
         fake_resp.content.readline.side_effect = side_effects + [b'']
@@ -81,14 +119,14 @@ class WatchTest(TestCase):
         fake_api.get_namespaces.__doc__ = ':return: V1NamespaceList'
 
         # Iteration must cease after all valid responses were received.
-        watch = Watch()
+        watch = k8s.watch.Watch(fake_api.get_namespaces)
         cnt = 0
-        async for _ in watch.stream(fake_api.get_namespaces): # noqa
+        async for _ in watch: # noqa
             cnt += 1
         self.assertEqual(cnt, len(side_effects))
 
     def test_unmarshal_with_float_object(self):
-        w = Watch()
+        w = k8s.watch.Watch(k8s.api.CoreV1Api().list_namespace)
         event = w.unmarshal_event('{"type": "ADDED", "object": 1}', 'float')
         self.assertEqual("ADDED", event['type'])
         self.assertEqual(1.0, event['object'])
@@ -96,7 +134,7 @@ class WatchTest(TestCase):
         self.assertEqual(1, event['raw_object'])
 
     def test_unmarshal_with_no_return_type(self):
-        w = Watch()
+        w = k8s.watch.Watch(k8s.api.CoreV1Api().list_namespace)
         event = w.unmarshal_event(
             '{"type": "ADDED", "object": ["test1"]}', None)
         self.assertEqual("ADDED", event['type'])
@@ -121,7 +159,8 @@ class WatchTest(TestCase):
             }
         }
 
-        ret = Watch().unmarshal_event(json.dumps(k8s_err), None)
+        watch = k8s.watch.Watch(k8s.api.CoreV1Api().list_namespace)
+        ret = watch.unmarshal_event(json.dumps(k8s_err), None)
         self.assertEqual(ret['type'], k8s_err['type'])
         self.assertEqual(ret['object'], k8s_err['object'])
         self.assertEqual(ret['object'], k8s_err['object'])
@@ -135,8 +174,8 @@ class WatchTest(TestCase):
         fake_api.get_namespaces.__doc__ = ':return: V1NamespaceList'
 
         with self.assertRaises(KeyError):
-            watch = Watch()
-            async for e in watch.stream(fake_api.get_namespaces, timeout_seconds=10): # noqa
+            watch = k8s.watch.Watch(fake_api.get_namespaces, timeout_seconds=10)
+            async for e in watch: # noqa
                 pass
 
 
