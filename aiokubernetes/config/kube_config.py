c567280a5edc0000bc3764b35ef53c223b3ea671
Oliver Nagy <olitheolix@gmail.com>
2018-07-03 15:57:09 +1000

    Added "new_websocket_client_from_config" function to kube_config.py.

--- aiokubernetes/config/kube_config.py
+++ aiokubernetes/config/kube_config.py
@@ -28,6 +28,7 @@ from requests_oauthlib import OAuth2Session
 from six import PY3
 
 from ..api_client import ApiClient, Configuration
+from ..stream import WebsocketApiClient
 from .config_exception import ConfigException
 from .dateutil import UTC, format_rfc3339, parse_rfc3339
 
@@ -492,3 +493,17 @@ def new_client_from_config(
                      client_configuration=client_config,
                      persist_config=persist_config)
     return ApiClient(configuration=client_config)
+
+
+def new_websocket_client_from_config(
+        config_file=None,
+        context=None,
+        persist_config=True):
+    """Loads configuration the same as load_kube_config but returns an ApiClient
+    to be used with any API object. This will allow the caller to concurrently
+    talk with multiple clusters."""
+    client_config = type.__call__(Configuration)
+    load_kube_config(config_file=config_file, context=context,
+                     client_configuration=client_config,
+                     persist_config=persist_config)
+    return WebsocketApiClient(configuration=client_config)
