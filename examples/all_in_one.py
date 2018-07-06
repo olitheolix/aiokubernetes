8a1a8a05f0b054a2c6d8b88bb25f7fbda21665da
Oliver Nagy <olitheolix@gmail.com>
2018-07-06 18:47:37 +1000

    Updated example: removed Websocket special case.

--- examples/all_in_one.py
+++ examples/all_in_one.py
@@ -10,6 +10,7 @@ more concise feature demonstrations.
 import asyncio
 import os
 
+import aiohttp
 import yaml
 
 import aiokubernetes as k8s
@@ -20,7 +21,7 @@ async def watch_resource(resource, **kwargs):
         print(f"{event.name} {event.obj.kind} {event.obj.metadata.name}")
 
 
-async def create_deployment(api_client, ws_api_client):
+async def create_deployment(api_client):
     img_alpine_34, img_alpine_35 = 'alpine:3.4', 'alpine:3.5'
     time_between_steps = 3
 
@@ -40,6 +41,7 @@ async def create_deployment(api_client, ws_api_client):
     k8s_v1beta = k8s.ExtensionsV1beta1Api(api_client)
     print(f'Creating deployment {name}...')
     resp = await k8s_v1beta.create_namespaced_deployment(body=body, namespace=namespace)
+    assert isinstance(resp.http, aiohttp.client_reqrep.ClientResponse)
     print('****', resp.http.method, resp.http.status, resp.http.url)
 
     # ---------- Patch ----------
@@ -54,6 +56,7 @@ async def create_deployment(api_client, ws_api_client):
     print(f'\nConnecting to a login pod: ', end='', flush=True)
     for i in range(300):
         resp = await k8s.CoreV1Api(api_client).list_namespaced_pod(namespace)
+        assert isinstance(resp.http, aiohttp.client_reqrep.ClientResponse)
         pods = resp.obj.items
         pods = [_ for _ in pods if _.metadata.name.lower().startswith('login')]
         pods = [_ for _ in pods if _.status.phase.lower() == 'running']
@@ -63,7 +66,6 @@ async def create_deployment(api_client, ws_api_client):
             await asyncio.sleep(0.1)
             continue
         print('\n****', resp.http.method, resp.http.status, resp.http.url, '\n')
-        del resp
 
         if len(pods) > 1:
             print('Found multiple login pods')
@@ -71,19 +73,20 @@ async def create_deployment(api_client, ws_api_client):
         pod_name = pods[0].metadata.name
         print(f'Connecting to Pod <{pod_name}>')
 
-        v1_ws = k8s.CoreV1Api(api_client=ws_api_client)
+        v1_ws = k8s.CoreV1Api(api_client=api_client)
         exec_command = [
             '/bin/sh', '-c',
             'echo This is stderr >&2; sleep 0s; echo This is stdout'
         ]
-        connection = await v1_ws.connect_get_namespaced_pod_exec(
+        websocket = await v1_ws.connect_get_namespaced_pod_exec(
             pod_name, namespace,
             command=exec_command,
             stderr=True, stdin=False,
             stdout=True, tty=False
         )
+        assert isinstance(websocket.http, aiohttp.client._WSRequestContextManager)
 
-        async with connection.http as ws:
+        async with websocket.http as ws:
             async for msg in ws:
                 print(f'  Websocket received: {msg.data}')
         break
@@ -99,6 +102,7 @@ async def create_deployment(api_client, ws_api_client):
     print(f'\nReplacing deployment {name}...')
     resp = await k8s_v1beta.replace_namespaced_deployment(
         name=name, namespace=namespace, body=body)
+    assert isinstance(resp.http, aiohttp.client_reqrep.ClientResponse)
     print('****', resp.http.method, resp.http.status, resp.http.url)
 
     # ---------- Delete ----------
@@ -110,6 +114,7 @@ async def create_deployment(api_client, ws_api_client):
     )
     resp = await k8s_v1beta.delete_namespaced_deployment(
         name=name, namespace=namespace, body=del_opts)
+    assert isinstance(resp.http, aiohttp.client_reqrep.ClientResponse)
     print('****', resp.http.method, resp.http.status, resp.http.url)
 
     print('------------ End of Demo ------------')
@@ -118,18 +123,16 @@ async def create_deployment(api_client, ws_api_client):
 async def setup():
     # Create client API instances (Websocket and Http).
     api_client = k8s.config.new_client_from_config()
-    ws_api_client = k8s.config.new_websocket_client_from_config()
 
     # Specify and dispatch the tasks.
     tasks = [
-        create_deployment(api_client, ws_api_client),
+        create_deployment(api_client),
         watch_resource(k8s.CoreV1Api(api_client).list_namespace, timeout_seconds=1),
     ]
     await asyncio.gather(*tasks)
 
     print('\nShutting down')
     await api_client.session.close()
-    await ws_api_client.session.close()
 
 
 def main():
