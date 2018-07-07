26b36299da20562f5c62e7cc9f722c3454a9e998
Oliver Nagy <olitheolix@gmail.com>
2018-07-07 21:23:16 +1000

    Improved "examples/all_in_one.py".

--- examples/all_in_one.py
+++ examples/all_in_one.py
@@ -27,7 +27,7 @@ async def create_deployment(api_client):
 
     # Create deployment.
     base_path = os.path.dirname(os.path.abspath(__file__))
-    fname = os.path.join(base_path, 'create-deployment.yaml')
+    fname = os.path.join(base_path, 'manifests/create-deployment.yaml')
     body = yaml.safe_load(open(fname, 'r'))
     name, namespace = body['metadata']['name'], body['metadata']['namespace']
     img_orig = body['spec']['template']['spec']['containers'][0]['image']
@@ -37,42 +37,60 @@ async def create_deployment(api_client):
     img_new = body['spec']['template']['spec']['containers'][0]['image']
     print(f'Replaced <{img_orig}> with <{img_new}>')
 
-    # ---------- Create ----------
+    # -------------------------------------------------------------------------
+    #                           Create Deployment
+    # -------------------------------------------------------------------------
     k8s_v1beta = k8s.ExtensionsV1beta1Api(api_client)
     print(f'Creating deployment {name}...')
     resp = await k8s_v1beta.create_namespaced_deployment(body=body, namespace=namespace)
     assert isinstance(resp.http, aiohttp.client_reqrep.ClientResponse)
-    print('****', resp.http.method, resp.http.status, resp.http.url)
+    print(' ->', resp.http.method, resp.http.status, resp.http.url)
 
-    # ---------- Patch ----------
+    # -------------------------------------------------------------------------
+    #                            Path Deployment
+    # -------------------------------------------------------------------------
     # print(f'Patching deployment {name}...')
-    # fname = os.path.join(base_path, 'patch-deployment.yaml')
+    # fname = os.path.join(base_path, 'manifests/patch-deployment.yaml')
     # body_patch = yaml.safe_load(open(fname, 'r'))
     # resp = await k8s_v1beta.patch_namespaced_deployment(
     #     name=name, namespace=namespace, body=body_patch)
     # await asyncio.sleep(time_between_steps)
 
-    # ---------- Connect to Pod ----------
+    # -------------------------------------------------------------------------
+    #                        Execute Command in Pod
+    # -------------------------------------------------------------------------
     print(f'\nConnecting to a login pod: ', end='', flush=True)
+
+    # Try several times to connect to the pod. We have to do this because we
+    # only just created the pod and it takes a few seconds until it is ready.
     for i in range(300):
+        # Get a list of all pods.
         resp = await k8s.CoreV1Api(api_client).list_namespaced_pod(namespace)
         assert isinstance(resp.http, aiohttp.client_reqrep.ClientResponse)
+
+        # Find all running pods whose name starts with 'login'.
         pods = resp.obj.items
         pods = [_ for _ in pods if _.metadata.name.lower().startswith('login')]
         pods = [_ for _ in pods if _.status.phase.lower() == 'running']
 
+        # Wait, rinse and repeat if Kubernetes returned an error or we found no
+        # running login Pod.
         if resp.http.status != 200 or len(pods) == 0:
             print('.', end='', flush=True)
             await asyncio.sleep(0.1)
             continue
-        print('\n****', resp.http.method, resp.http.status, resp.http.url, '\n')
+        print('\n ->', resp.http.method, resp.http.status, resp.http.url, '\n')
 
+        # Could be a stale deployment - not a problem, but let user know.
         if len(pods) > 1:
             print('Found multiple login pods')
 
+        # Extract the pod name that we will connect to.
         pod_name = pods[0].metadata.name
         print(f'Connecting to Pod <{pod_name}>')
 
+        # Connect to the pod and run a few shell commands. This will return a
+        # Websocket connection that we will consume afterwards.
         v1_ws = k8s.CoreV1Api(api_client=api_client)
         exec_command = [
             '/bin/sh', '-c',
@@ -86,6 +104,8 @@ async def create_deployment(api_client):
         )
         assert isinstance(websocket.http, aiohttp.client._WSRequestContextManager)
 
+        # Consume the Websocket until all commands have finished and Kubernetes
+        # closes the connection.
         async with websocket.http as ws:
             async for msg in ws:
                 print(f'  Websocket received: {msg.data}')
@@ -93,7 +113,9 @@ async def create_deployment(api_client):
     else:
         print('No login has entered "running" state yet - skip connection test')
 
-    # ---------- Replace ----------
+    # -------------------------------------------------------------------------
+    #                          Replace Deployment
+    # -------------------------------------------------------------------------
     img_orig = body['spec']['template']['spec']['containers'][0]['image']
     body['spec']['template']['spec']['containers'][0]['image'] = img_alpine_35
     img_new = body['spec']['template']['spec']['containers'][0]['image']
@@ -103,9 +125,11 @@ async def create_deployment(api_client):
     resp = await k8s_v1beta.replace_namespaced_deployment(
         name=name, namespace=namespace, body=body)
     assert isinstance(resp.http, aiohttp.client_reqrep.ClientResponse)
-    print('****', resp.http.method, resp.http.status, resp.http.url)
+    print(' ->', resp.http.method, resp.http.status, resp.http.url)
 
-    # ---------- Delete ----------
+    # -------------------------------------------------------------------------
+    #                           Delete Deployment
+    # -------------------------------------------------------------------------
     await asyncio.sleep(time_between_steps)
     print(f'\nDeleting deployment {name}...')
     del_opts = k8s.V1DeleteOptions(
@@ -115,7 +139,7 @@ async def create_deployment(api_client):
     resp = await k8s_v1beta.delete_namespaced_deployment(
         name=name, namespace=namespace, body=del_opts)
     assert isinstance(resp.http, aiohttp.client_reqrep.ClientResponse)
-    print('****', resp.http.method, resp.http.status, resp.http.url)
+    print(' ->', resp.http.method, resp.http.status, resp.http.url)
 
     print('------------ End of Demo ------------')
 
