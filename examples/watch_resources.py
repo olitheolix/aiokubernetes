552f456aa93730e0c0c8c68f304a61d9fbce25bd
Oliver Nagy <olitheolix@gmail.com>
2018-07-19 10:55:48 +1000

    Updated examples to use new watch.

--- examples/watch_resources.py
+++ examples/watch_resources.py
@@ -1,37 +1,51 @@
 """Watch multiple K8s event streams without threads."""
 import asyncio
+import os
+import warnings
 
 import aiokubernetes as k8s
 
 
-async def watch_resource(resource, **kwargs):
+async def watch_resource(client, cargs):
     # Consume and print the events as they stream in from the `resource`
-    async for event in k8s.Watch(resource, **kwargs):
+    watch = k8s.watch.Watch2(client.request(**cargs))
+    async for event in watch:
         print(f"{event.name} {event.obj.kind} {event.obj.metadata.name}")
 
 
 async def main():
     # Create a client instance and load the credentials from ~/.kube/kubeconfig
-    api_client = k8s.config.new_client_from_config()
+    kubeconf = os.path.expanduser(os.environ.get('KUBECONFIG', '~/.kube/config'))
+    client_config = k8s.configuration.Configuration()
+    with warnings.catch_warnings(record=True):
+        k8s.config.kube_config.load_kube_config(
+            config_file=kubeconf,
+            client_configuration=client_config,
+            persist_config=False
+        )
+    client = k8s.clients.make_aiohttp_client(client_config)
+    proxy = k8s.api_proxy.Proxy(client_config)
 
     # Namespaces and Pods are part of the K8s Core API.
-    corev1 = k8s.CoreV1Api(api_client)
+    corev1 = k8s.CoreV1Api(proxy)
 
     # Deployments are part of the Extension API (still in beta).
-    extv1beta = k8s.ExtensionsV1beta1Api(api_client)
+    extv1beta = k8s.ExtensionsV1beta1Api(proxy)
+
+    cargs_ns = corev1.list_namespace(timeout_seconds=5, watch=True)
+    cargs_pods = corev1.list_pod_for_all_namespaces(timeout_seconds=5, watch=True)
+    cargs_deploy = extv1beta.list_deployment_for_all_namespaces(timeout_seconds=5, watch=True)
 
     # Specify and dispatch the tasks.
     tasks = [
-        watch_resource(corev1.list_namespace, timeout_seconds=5),
-        watch_resource(corev1.list_pod_for_all_namespaces, timeout_seconds=5),
-        watch_resource(
-            extv1beta.list_deployment_for_all_namespaces, timeout_seconds=5
-        ),
+        watch_resource(client, cargs_ns),
+        watch_resource(client, cargs_pods),
+        watch_resource(client, cargs_deploy),
     ]
     await asyncio.gather(*tasks)
 
     # Close all pending connections.
-    await api_client.close()
+    await client.close()
 
 
 if __name__ == '__main__':
