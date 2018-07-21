e2861f9c29ed75c2e5f8ab38ae35497f3e308fe6
Oliver Nagy <olitheolix@gmail.com>
2018-07-21 18:03:23 +1000

    Docu updates.

--- aiokubernetes/clients.py
+++ aiokubernetes/clients.py
@@ -5,21 +5,37 @@ import requests
 
 
 def get_aiohttp(config):
-    assert config.ssl_ca_cert is not None
-    ca_certs = config.ssl_ca_cert
-    ssl_context = ssl.create_default_context(cafile=ca_certs)
+    """Return a configured AioHttp client.
 
+    NOTE: this is a convenience function only to reduce boiler plate.
+
+    Inputs:
+        config: k8s.configuration.Config instance
+            Typically you pass it whatever `k8s.utils.load_config()` returns.
+
+    Returns:
+        Requests session instance.
+    """
     connector = aiohttp.TCPConnector(
         limit=4,
-        ssl_context=ssl_context,
-        verify_ssl=config.verify_ssl
+        ssl_context=ssl.create_default_context(cafile=config.ssl_ca_cert),
+        verify_ssl=config.verify_ssl  # Bool, usually True.
     )
     return aiohttp.ClientSession(connector=connector)
 
 
 def get_requests(config):
-    assert config.ssl_ca_cert is not None
+    """Return a configured Requests client.
 
+    NOTE: this is a convenience function only to reduce boiler plate.
+
+    Inputs:
+        config: k8s.configuration.Config instance
+            Typically you pass it whatever `k8s.utils.load_config()` returns.
+
+    Returns:
+        Requests session instance.
+    """
     sess = requests.Session()
     sess.verify = config.ssl_ca_cert
     return sess
