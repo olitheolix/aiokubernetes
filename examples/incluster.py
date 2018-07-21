8d42ac1e0b76ebd2c2f1a40a35fddebfb8ffa612
Oliver Nagy <olitheolix@gmail.com>
2018-07-21 16:24:41 +1000

    Updated 'incluster' example.

--- examples/incluster.py
+++ examples/incluster.py
@@ -1,7 +1,7 @@
 """Use service account credentials to list all pods in the cluster.
 
 NOTE: must run inside a Pod with the correct service account credentials. See
-`deploy-pod-with-sa.yaml` for an example to set this up.
+`manifests/deploy-pod-with-sa.yaml` for an example to set this up.
 """
 import asyncio
 
@@ -9,15 +9,14 @@ import aiokubernetes as k8s
 
 
 async def main():
-    # Assume we are inside a Pod and load its service account credentials.
-    client_config = k8s.config.incluster_config.load_service_account_config()
+    # Assume we are inside a Pod. Load its service account credentials.
+    config = k8s.config.incluster_config.load()
+    client = k8s.clients.get_aiohttp(config)
+    proxy = k8s.api_proxy.Proxy(config)
 
-    # Create a client with the service account credentials.
-    api_client = k8s.api_client.ApiClient(configuration=client_config)
-
-    # Ask for all Pods - no different than when running locally.
-    v1 = k8s.api.CoreV1Api(api_client)
-    ret = await v1.list_pod_for_all_namespaces()
+    # Ask for all Pods in the cluster.
+    cargs = k8s.api.CoreV1Api(proxy).list_pod_for_all_namespaces()
+    ret = await client.request(**cargs)
     assert ret.http.status == 200
 
     # Print the pod names.
@@ -25,7 +24,7 @@ async def main():
         print(f"{i.metadata.namespace} {i.metadata.name}")
 
     # Close all pending connections.
-    await api_client.close()
+    await client.close()
 
 
 if __name__ == '__main__':
