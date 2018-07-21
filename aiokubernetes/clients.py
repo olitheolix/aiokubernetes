2ce8b30fcf3930c5e8962d3556c50bc03dc4568b
Oliver Nagy <olitheolix@gmail.com>
2018-07-21 17:51:54 +1000

    Added `clients.get_requests`.

--- aiokubernetes/clients.py
+++ aiokubernetes/clients.py
@@ -1,16 +1,25 @@
 import ssl
 
 import aiohttp
+import requests
 
 
-def get_aiohttp(configuration):
-    assert configuration.ssl_ca_cert is not None
-    ca_certs = configuration.ssl_ca_cert
+def get_aiohttp(config):
+    assert config.ssl_ca_cert is not None
+    ca_certs = config.ssl_ca_cert
     ssl_context = ssl.create_default_context(cafile=ca_certs)
 
     connector = aiohttp.TCPConnector(
         limit=4,
         ssl_context=ssl_context,
-        verify_ssl=configuration.verify_ssl
+        verify_ssl=config.verify_ssl
     )
     return aiohttp.ClientSession(connector=connector)
+
+
+def get_requests(config):
+    assert config.ssl_ca_cert is not None
+
+    sess = requests.Session()
+    sess.verify = config.ssl_ca_cert
+    return sess
